dcfcc3df75c47ed74474592e9eac4b11a0cf78c9
Wesley Campbell <wes@et.byu.edu>
2026-04-23 15:06:08 -0600

    DEVELOPMENT: Implemented DAO methods for Habits_Catalog.
    Getters for entries in the Habits_Catalog database are now implemented.
    
    Removed previous, outdated boilerplate code for other DAO objects.

--- server/src/main/java/dataaccess/habits/HabitsDAO.java
+++ server/src/main/java/dataaccess/habits/HabitsDAO.java
@@ -3,57 +3,95 @@ package dataaccess.habits;
 import dataaccess.SQLDatabaseDAO;
 import dataaccess.exception.*;
 
+import org.jooq.*;
+import org.jooq.impl.*;
+import org.jooq.Record;
+import org.jooq.types.*;
+import static org.jooq.impl.DSL.*;
+
+import static jooq.habits.Tables.*;
+import jooq.habits.tables.pojos.Habits_Catalog;
+
 import java.sql.SQLException;
+import java.sql.*;
+
+import java.util.List;
 
 public class HabitsDAO extends SQLDatabaseDAO {
-	//
-	// ===================== DATABASE STATEMENTS ===========================
-	//
-	
-	private static final String TABLE_NAME = "habits__catalog";
-
-	private static final String DB_SELECT_HABIT_STATEMENT_NAME = String.format("""
-			SELECT * FROM %s WHERE name=?""", TABLE_NAME); 
-
-	private static final String DB_SELECT_HABIT_STATEMENT_ID = String.format("""
-			SELECT * FROM %s WHERE id=?""", TABLE_NAME); 
-	
-	private static final String DB_INSERT_HABIT_STATEMENT = String.format("""
-			INSERT INTO %s (name, description, is_active) VALUES (?, ?, ?)"""
-			, TABLE_NAME);
-
-	private static final String DB_CLEAR_HABIT_TABLE_STATEMENT = String.format("""
-			TRUNCATE TABLE %s""",
-			TABLE_NAME);
-
-	private static final String DB_DELETE_HABIT_STATEMENT_ID = String.format("""
-			DELETE FROM %s WHERE id=?""",
-			TABLE_NAME);
-
-	private static final String DB_DELETE_HABIT_STATEMENT_NAME = String.format("""
-			DELETE FROM %s WHERE name=?""",
-			TABLE_NAME);
-
-	private static final String DB_CHECK_HABIT_EXIST_STATEMENT_NAME = String.format("""
-		SELECT 1 FROM %s WHERE name=?""",
-		TABLE_NAME);
-
-	private static final String DB_CHECK_HABIT_EXIST_STATEMENT_ID = String.format("""
-		SELECT 1 FROM %s WHERE id=?""",
-		TABLE_NAME);
-
-	private static final String DB_SET_COL_VAL_STATEMENT= String.format("""
-			UPDATE %s SET """, TABLE_NAME) + "%s=? WHERE habit_id=?";
-
 	//
 	// ======================== CONSTRUCTORS =========================
 	//
 	
 	public HabitsDAO(HabitsDatabaseManager dbManager) throws DataAccessException {
-		super(TABLE_NAME, dbManager);
+		super(dbManager);
+
+		System.out.println(">>> " + getHabitCatalog().toString());
 	}
 
 	//
 	// ======================== DATA ACCESS METHODS =============================
 	//
+
+	/**
+	 * Given a habitID, this method will try and fetch the associated habit_catalog object from the database. If not found, will throw a `ObjectNotFoundException`.
+	 *
+	 * @param habitID The habit_id of the desired habit_catalog entry.
+	 *
+	 * @return The Habit_Catalog object
+	 */
+	public Habits_Catalog getHabit(int habitID) throws DataAccessException {
+		ULong id = ULong.valueOf(habitID);
+
+		// Open the db connection
+		Habits_Catalog habit = this.executeStatement(
+			ctx ->
+				ctx.select()
+				.from(HABITS__CATALOG)
+				.where(HABITS__CATALOG.HABIT_ID.eq(id))
+				.fetchOneInto(Habits_Catalog.class)
+		);
+
+		// If it doesn't exist, throw an exception to notify caller.
+		if (habit == null) {
+			throw new ObjectNotFoundException(
+			String.format("Habit with ID %d not found in database", habitID));
+		}
+		
+		return habit;
+	}
+
+	/**
+	 * Returns the entire habit catalog from the database
+	 *
+	 * @return A list containing all habit entries in the database
+	 */
+	public List<Habits_Catalog> getHabitCatalog() throws DataAccessException {
+		return this.executeStatement(
+			ctx -> ctx.select()
+					.from(HABITS__CATALOG)
+					.fetchInto(Habits_Catalog.class)
+		);
+	}
+
+	/**
+	 * Returns the entire habit catalog from the database
+	 *
+	 * @param limit The upper limit of entries to return. 
+	 * @param offset Where to start the selection
+	 * 
+	 * @return A List containing all habit entries in the database.
+	 */
+	public List<Habits_Catalog> getHabitCatalog(int limit, int offset) throws DataAccessException {
+		List<Habits_Catalog> catalog = this.executeStatement(
+			ctx -> 
+				ctx.select()
+				.from(HABITS__CATALOG)
+				.orderBy(HABITS__CATALOG.HABIT_ID.asc())
+				.limit(limit)
+				.offset(offset)
+				.fetchInto(Habits_Catalog.class)
+		);
+
+		return catalog;
+	}	
 }
--- server/src/main/java/dataaccess/habits/StatsDAO.java
+++ server/src/main/java/dataaccess/habits/StatsDAO.java
@@ -5,30 +5,10 @@ import dataaccess.exception.*;
 
 public class StatsDAO extends SQLDatabaseDAO {
 	//
-	// ================= DATABASE STATEMENTS ==================
-	//
-	
-	private static final String TABLE_NAME = "habits__stats";
-
-	private static final String DB_SELECT_STAT_STATEMENT = String.format("""
-			SELECT * FROM %s WHERE habit_id=?""",
-			TABLE_NAME);
-
-	private static final String DB_CLEAR_STAT_TABLE_STATEMENT = String.format("""
-			TRUNCATE TABLE %s""",
-			TABLE_NAME);
-
-	private static final String DB_CHECK_STAT_EXIST_STATEMENT = String.format("""
-			SELECT 1 FROM %s WHERE habit_id=?""",
-			TABLE_NAME);
-
-	private static final String DB_SET_COL_VAL_STATEMENT= String.format("""
-			UPDATE %s SET """, TABLE_NAME) + "%s=? WHERE habit_id=?";
-	//
 	// ======================= CONSTRUCTORS ==========================
 	//
 	
 	public StatsDAO(HabitsDatabaseManager dbManager) throws DataAccessException {
-		super(TABLE_NAME, dbManager);
+		super(dbManager);
 	}
 }
