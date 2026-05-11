6a10dc0b93688acf1643032a5678aef1d18894af
Wesley Campbell <wes@et.byu.edu>
2026-05-11 14:51:43 -0600

    DEVELOPMENT: Continued development on Habit Stats API service level

---
+++ server/src/test/java/service/habits/GetHabitHistoryServiceTests.java
@@ -0,0 +1,54 @@
+package service.habits;
+
+import dataaccess.habits.*;
+import dataaccess.exception.*;
+
+import api.habits.*;
+
+import org.junit.jupiter.api.*;
+
+import static org.mockito.Mockito.*;
+
+public class GetHabitHistoryServiceTests {
+	//
+	// ======================== GLOBALS =======================
+	//
+	
+	//
+	// ======================== TEST INITIALIZATION ============	
+	//
+	
+	//
+	// ====================== UNIT TESTS =====================
+	//
+	
+	@Test
+	public void GetHabitStatsTest_Correct() {
+
+	}
+
+	@Test
+	public void GetHabitStatsTest_Incorrect() {
+
+	}
+
+	@Test 
+	public void GetHabitGoalTest_Correct() {
+
+	}
+
+	@Test
+	public void getHabitGoalTest_Incorrect() {
+
+	}
+
+	@Test
+	public void getHabitStreakTest_Correct() {
+
+	}
+
+	@Test
+	public void getHabitStreakTest_Incorrect() {
+
+	}
+}
